328ff22ba2aeae790efca2c35a6a22f7426bd353
bomoge <45061097+bomoge@users.noreply.github.com>
2019-06-04 10:36:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let btnSTART=document.getElementById("START");
 function closeMenu() {
 	let menu=document.getElementById("menu");
 	menu.style.display='none'; 
-}btnSTART.onclick=closeMenu;
+}
+btnSTART.addEventListener('click', closeMenu);
 
 let resButton=document.getElementById("reset");
 function restart(){
@@ -13,7 +14,6 @@ function random(min,max) {
 	var num = Math.floor(Math.random()*(max-min)) + min;	
 	return num;
 }
-
 const szer = 700
 const wys = 600
 const wiersze = 5
@@ -73,29 +73,31 @@ class Blok {
     ctx.fillRect(
       this.x,
       this.y,
-      Blok.width,
-      Blok.height,
+      Blok.szer,
+      Blok.wys,
     )
     ctx.strokeStyle = '#ffffff'
     ctx.strokeRect(
       this.x,
       this.y,
-      Blok.width,
-      Blok.height,
+      Blok.szer,
+      Blok.wys,
     )
   }
 }
+
 Blok.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
-Blok.width = szer / bloczki - 2 * przerwa
-Blok.height = 33
+Blok.szer = szer / bloczki - 2 * przerwa
+Blok.wys = 33
+
 
 const tworzBloki = () => {
   const bloki = []
   for (let i = 0; i < wiersze; i++) {
     bloki[i] = []
     for (let j = 0; j < bloczki; j++) {
-      const x = (2 * j + 1) * przerwa + j * Blok.width
-      const y = (2 * i + 1) * przerwa + i * Blok.height
+      const x = (2 * j + 1) * przerwa + j * Blok.szer
+      const y = (2 * i + 1) * przerwa + i * Blok.wys
       bloki[i][j] = new Blok(x, y)
     }
   }
@@ -111,16 +113,56 @@ const rysujBloki = (bloki, ctx) => {
   }
 }
 
+class Pilka {
+  constructor() {
+    this.x = szer / 2
+    this.y = wys - Pilka.radius - Platforma.wys
+    this.angle = -(Math.random() * (Math.PI / 2) + Math.PI / 4)
+  }
+
+  draw(ctx) {
+    ctx.beginPath()
+    ctx.arc(
+      this.x,
+      this.y,
+      Pilka.radius, 0, 2 * Math.PI,
+      false
+    )
+    ctx.fillStyle = Pilka.color
+    ctx.fill()
+  }
+}
+
+Pilka.color = '#ffff00'
+Pilka.radius = 10
+Pilka.predk = 2.5
+
+
+const core = (arkanoid) => {
+  const {
+    platforma,
+    bloki,
+	pilka,	
+  } = arkanoid
+
+  if (pilka.y <= Pilka.radius) {
+    Pilka.predk = -Pilka.predk
+    return
+  }
+}
+
 
 const render = (ctx, arkanoid) => {
   const {
     platforma,
-	bloki,
+    bloki,
+	pilka,	
   } = arkanoid
   
   ctx.clearRect(0, 0, szer, wys)
-  platforma.draw(ctx)
   rysujBloki(bloki, ctx)
+  platforma.draw(ctx)
+  pilka.draw(ctx)
 
   core(arkanoid)
 
@@ -129,20 +171,20 @@ const render = (ctx, arkanoid) => {
   }
 }
 
-
-window.onload = () => {
+btnSTART.onclick = () => {
   const canvas = document.getElementById('game')
   const ctx = canvas.getContext('2d')
 
   const arkanoid = {
     platforma: new Platforma(),
 	bloki: tworzBloki(),
+	pilka: new Pilka(),
     status: 'play',
     finish: () => {
-      ctx.font = '50px Arial'
+      ctx.font = 'bold 70px Arial'
       ctx.fillStyle = 'red'
       ctx.textAlign = 'center'
-      ctx.fillText('GAME OVER', szer / 2, wys / 2)
+      ctx.fillText('G A M E    O V E R !', szer / 2, wys / 2)
     },
   }
 
